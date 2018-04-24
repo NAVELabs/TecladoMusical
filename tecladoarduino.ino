8e13f3a757b233f3d812b23c0d98436cb0629b81
Antoanne Pontes <antoanne@gmail.com>
2018-04-24 12:05:23 -0300

    Versão para oficinas

---
+++ tecladoarduino.ino
@@ -0,0 +1,61 @@
+#include <CapacitiveSensor.h>
+
+const int comparationValue = 30;
+const int inputPin = 2;
+const int buzzerPin = 10;
+const int note1 = 262;
+const int note2 = 294;
+const int note3 = 330;
+const int note4 = 350;
+const int note5 = 392;
+long sensorValue1 = 0;
+long sensorValue2 = 0;
+long sensorValue3 = 0;
+long sensorValue4 = 0;
+long sensorValue5 = 0;
+
+CapacitiveSensor capSensor1 = CapacitiveSensor(inputPin,3);
+CapacitiveSensor capSensor2 = CapacitiveSensor(inputPin,4);
+CapacitiveSensor capSensor3 = CapacitiveSensor(inputPin,5);
+CapacitiveSensor capSensor4 = CapacitiveSensor(inputPin,6);
+CapacitiveSensor capSensor5 = CapacitiveSensor(inputPin,7);
+
+void setup() {
+  
+}
+
+void loop() {
+  sensorValue1 = capSensor1.capacitiveSensor(30);
+  sensorValue2 = capSensor2.capacitiveSensor(30);
+  sensorValue3 = capSensor3.capacitiveSensor(30);
+  sensorValue4 = capSensor4.capacitiveSensor(30);
+  sensorValue5 = capSensor5.capacitiveSensor(30);
+
+ if(sensorValue1 > comparationValue){
+  BuzzerSound(note1);
+ }
+ 
+ else if(sensorValue2 > comparationValue){
+  BuzzerSound(note2);
+ }
+  
+ else if(sensorValue3 > comparationValue){
+  BuzzerSound(note3);
+ }
+ 
+ else if(sensorValue4 > comparationValue){
+  BuzzerSound(note4);
+ }
+ 
+ else if(sensorValue5 > comparationValue){
+  BuzzerSound(note5);
+ }
+  else{
+    noTone(buzzerPin);
+  }
+}
+
+
+void BuzzerSound(int note){
+  tone(buzzerPin,note);
+}
